6b6f5592bc87684cd0174b40e3fba18e0555bc51
Daniel Niecke <daniel@niecke-it.de>
2026-03-14 20:33:49 +0100

    add vm with postgres and redis

--- terraform/modules/apis/main.tf
+++ terraform/modules/apis/main.tf
@@ -12,3 +12,8 @@ resource "google_project_service" "artifactregistry" {
   service            = "artifactregistry.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "secretmanager" {
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
+}
---
+++ terraform/modules/secrets/main.tf
@@ -0,0 +1,18 @@
+resource "random_password" "postgres" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "google_secret_manager_secret" "postgres_password" {
+  secret_id = "lumios-postgres-password"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "postgres_password" {
+  secret      = google_secret_manager_secret.postgres_password.id
+  secret_data = random_password.postgres.result
+}
